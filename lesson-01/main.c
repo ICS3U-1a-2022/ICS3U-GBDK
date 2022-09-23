b232b8ba420b2aca58e1229fb114a71e2f97955e
ICS3U.1.2022 <ics3u.1.2022@mths.ca>
2022-09-23 16:16:17 +0000

    all new files

---
+++ lesson-01/main.c
@@ -0,0 +1,12 @@
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by: Mr. Coxall
+// Created on: Sept 2020
+// This program is the Space Aliens game for the GameBoy
+
+#include <gb/gb.h>
+#include <stdio.h>
+
+void main() {
+    printf("Hello, World!");
+}
